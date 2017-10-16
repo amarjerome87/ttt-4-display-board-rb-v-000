c8519464145eb2baa90c2237a1ecb668e87feb9f
Amar Chatman <amarjerome@gmail.com>
2017-10-16 08:54:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,45 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)
+board = [" "," "," "," ","X"," "," "," "," "]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)
+board = ["O"," "," "," "," "," "," "," "," "]
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)
+board = ["O"," "," "," ","X"," "," "," "," "]
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)
+board = ["X","X","X"," "," "," "," "," "," "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board
