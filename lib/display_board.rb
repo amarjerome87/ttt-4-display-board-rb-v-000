840f4a7bc6550999891a33217357dc557e4da59c
Amar Chatman <amarjerome@gmail.com>
2017-10-16 08:56:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -52,3 +52,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board(board)
+board = ["X"," "," "," ","X"," "," "," ","X"]
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   | #{board[8]}  "
+end
+display_board(board)
