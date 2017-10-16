10de8178f7e5367ee3c8a1f773cd52f3f12215a0
Amar Chatman <amarjerome@gmail.com>
2017-10-16 08:59:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -61,3 +61,39 @@ def display_board(board)
   puts "   |   | #{board[8]}  "
 end
 display_board(board)
+board = [" "," ","O"," ","O"," ","O"," "," "]
+def display_board(board)
+  puts "   |   | #{board[2]}  "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts " #{board[6]} |   |   "
+end
+display_board(board)
+board = ["X"," "," "," ","O"," "," "," ","O"]
+def display_board(board)
+  puts " #{board[0]} |   |   "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts "   |   | #{board[8]}  "
+end
+display_board(board)
+board = ["X","X","X","X","X","X","X","X","X"]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]}  | #{board[2]}  "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}  "
+end
+display_board(board)
+board = ["O","O","O","O","O","O","O","O","O"]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]}  | #{board[2]}  "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}  "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}  "
+end
+display_board(board)
